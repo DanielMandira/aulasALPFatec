373576779709a066315d09904b301c0c6adae323
Daniel Augusto Mandira <105872910+DanielMandira@users.noreply.github.com>
2023-09-15 21:09:57 -0300

    Add files via upload

---
+++ Aula 01/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int x = 10;
+    int y = 12;
+    double d = 10.33;
+    float z = 11;
+    int soma = x + y -1;
+    char c[] = "Socorro";
+    printf("a soma de X e Y - 1 eh %d\n", soma);
+    printf("float eh %.1f",z);
+    printf("\n double eh %.1f",d);
+    printf("\n texto eh %s",c);
+    return 0;
+}
