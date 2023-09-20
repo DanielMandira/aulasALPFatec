c2dabbe897973e27d295df3e2a4bab78db0acb28
Daniel Augusto Mandira <105872910+DanielMandira@users.noreply.github.com>
2023-09-20 16:34:21 -0300

    atividade 7 20/09

--- Atividade 01/main.c
+++ Atividade 01/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 int main()
 {
     double r;
     double pi = 3.14159;
-    printf("Calcule o volume da esfera\nDigite o valor do raio: ");
+    printf("Calcule o volume da esfera\nDigite o valor do raio em CM: ");
     scanf("%lf",&r);
-    double vol = (pi* pow(2,r));
+    double vol = (4 * pi * pow(r,3)/3);
     printf("\nO volume da esfera eh de: %.2lf", vol);
-    printf("metros cubicos");
+    printf(" cm3");
     return 0;
 }
