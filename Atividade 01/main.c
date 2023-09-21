a3bbba01d6bdc954f2444fa0d6d24039b88d2d1a
DSM-1 <dsm1@fatec.academico>
2023-09-21 20:16:41 -0300

    Atividade Finalizada

--- Atividade 01/main.c
+++ Atividade 01/main.c
@@ -4,17 +4,25 @@
 int main()
 {
     double valor, horaE, horaS, minE, minS, taxa;
-    taxa = 0,066;
-    printf("Calcule o valor do estacionamento\nDigite apenas a Hora de entrada 'Padrão 24h': ");
+    taxa = 4;
+    printf("Calcule o valor do estacionamento\nDigite apenas a Hora de entrada 'Padrao 24h': ");
     scanf("%lf", &horaE);
     printf("Digite os minutos de entrada: ");
     scanf("%lf", &minE);
-    printf("\nDigite apenas a Hora de saida 'Padrão 24h': ");
+    printf("\nDigite apenas a Hora de saida 'Padrao 24h': ");
     scanf("%lf", &horaS);
     printf("Digite os minutos de saida: ");
     scanf("%lf", &minS);
-    valor = ((((horaS - horaE)*4) + (minS-minE)* taxa));
-    printf("O valor do estacionamento sera de: %0.2lf", valor);
+    if(horaS == 00){
+        horaS = 24;
+        valor = ((((minS/60)+horaS)-((minE/60)+horaE))*taxa);
+        printf("O valor do estacionamento sera de: %0.2lf", valor);
+    }
+    else{
+        valor = ((((minS/60)+horaS)-((minE/60)+horaE))*taxa);
+        printf("O valor do estacionamento sera de: %0.2lf", valor);
+
+    }
     return 0;
 
 }
